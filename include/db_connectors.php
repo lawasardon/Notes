a564c72d70f9f42abf39aab25c51d8c8180a7984
4Rd0n <125838445+lawasardon@users.noreply.github.com>
2024-09-25 22:29:39 +0800

    Update db_connectors.php

--- include/db_connectors.php
+++ include/db_connectors.php
@@ -1,12 +1,13 @@
 <?php
 function connectDB() {
-    $host = "localhost";
-    $dbname = "noteapp";
-    $username = "root";
-    $password = "";
+    $host = getenv('DB_HOST');
+    $dbname = getenv('DB_DATABASE');
+    $username = getenv('DB_USERNAME');
+    $password = getenv('DB_PASSWORD');
+    $port = getenv('DB_PORT') ?: '3306';
 
     try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     } catch (PDOException $e) {
@@ -15,4 +16,5 @@ function connectDB() {
 
     return null;
 }
+
 ?>
